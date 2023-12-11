d62ea1d25b90a4a77ae3cf0069748798508543d7
nikeedev <nikita.gon.dev@gmail.com>
2023-12-11 17:59:47 +0100

    up

---
+++ README.md
@@ -0,0 +1,10 @@
+# Rust-OS
+
+As learned from: [https://os.phil-opp.com/](https://os.phil-opp.com/)
+
+Run using `cargo run`
+
+Requirements:
+- Nigtly edition of Rust
+- `bootimage` (installable using `cargo install bootimage` command)
+- And bunch of other stuff, instructions in the website I referenced.
---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
+    loop {}
+}
+
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
